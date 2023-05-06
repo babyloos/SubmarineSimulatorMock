2dd7625a288ff7d9c3ebf7d56dcec9f0a315e42b
babyloos <babyloos2501@gmail.com>
2023-05-06 17:04:14 +0900

    ShipControllerBaseを追加

---
+++ Assets/Scripts/EnemyShipController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShipController : ShipControllerBase
+{
+  private float _distCourse;
+
+  void Start()
+  {
+  }
+
+  void Update()
+  {
+  }
+}
