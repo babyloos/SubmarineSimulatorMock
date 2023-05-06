bc3b2a9eee263470d9adb622bcc927b0c4f25755
babyloos <babyloos2501@gmail.com>
2023-05-06 18:01:26 +0900

    商船の初期方向修正

--- Assets/Scripts/EnemyShipManager.cs
+++ Assets/Scripts/EnemyShipManager.cs
@@ -7,9 +7,9 @@ public class EnemyShipManager : MonoBehaviour
   public GameObject CargoShipPrefab;
 
   void Start () {
-    Instantiate(this.CargoShipPrefab, new Vector3(-10.0f,0.0f,-200.0f), Quaternion.identity);
-    Instantiate(this.CargoShipPrefab, new Vector3(-10.0f,0.0f,-500.0f), Quaternion.identity);
-    Instantiate(this.CargoShipPrefab, new Vector3(-10.0f,0.0f,-700.0f), Quaternion.identity);
+    Instantiate(this.CargoShipPrefab, new Vector3(-10.0f,0.0f,-200.0f), Quaternion.Euler(0, 90f, 0));
+    Instantiate(this.CargoShipPrefab, new Vector3(-10.0f,0.0f,-500.0f), Quaternion.Euler(0, 90f, 0));
+    Instantiate(this.CargoShipPrefab, new Vector3(-10.0f,0.0f,-700.0f), Quaternion.Euler(0, 90f, 0));
   }
 
   void Update()
