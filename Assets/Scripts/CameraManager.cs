2b6bf27fd0c5257d72ff1c1f51646286370b90da
babyloos <babyloos2501@gmail.com>
2024-07-20 15:28:48 +0900

    リファクタ

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -24,42 +24,26 @@ public class CameraManager : MonoBehaviour
     }
     else if (Input.GetMouseButton(0))
     {
-      if (!reverse)
-      {
-        var x = (lastMousePosition.x - Input.mousePosition.x);
-        var y = (lastMousePosition.y - Input.mousePosition.y);
-
-        if (Mathf.Abs(x) < Mathf.Abs(y))
-          x = 0;
-        else
-          y = 0;
-
-        var newAngle = Vector3.zero;
-        newAngle.x = x * rotationSpeed.x;
-        newAngle.y = y * rotationSpeed.y;
-
-        mainCamera.transform.RotateAround(playerObject.transform.position, Vector3.up, newAngle.x);
-        mainCamera.transform.RotateAround(playerObject.transform.position, mainCamera.transform.right, -newAngle.y);
-        lastMousePosition = Input.mousePosition;
+      var speed = rotationSpeed;
+      if (reverse) {
+        speed *= -1;
       }
+
+      var x = (lastMousePosition.x - Input.mousePosition.x);
+      var y = (lastMousePosition.y - Input.mousePosition.y);
+
+      if (Mathf.Abs(x) < Mathf.Abs(y))
+        x = 0;
       else
-      {
-        var x = (Input.mousePosition.x - lastMousePosition.x);
-        var y = (lastMousePosition.y - Input.mousePosition.y);
+        y = 0;
 
-        if (Mathf.Abs(x) < Mathf.Abs(y)) 
-          x = 0; 
-        else 
-          y = 0; 
+      var newAngle = Vector3.zero;
+      newAngle.x = x * speed.x;
+      newAngle.y = y * speed.y;
 
-        var newAngle = Vector3.zero;
-        newAngle.x = x * rotationSpeed.x;
-        newAngle.y = y * rotationSpeed.y;
-
-        mainCamera.transform.RotateAround(playerObject.transform.position, Vector3.up, newAngle.x);
-        mainCamera.transform.RotateAround(playerObject.transform.position, transform.right, newAngle.y);
-        lastMousePosition = Input.mousePosition;
-      }
+      mainCamera.transform.RotateAround(playerObject.transform.position, Vector3.up, newAngle.x);
+      mainCamera.transform.RotateAround(playerObject.transform.position, mainCamera.transform.right, -newAngle.y);
+      lastMousePosition = Input.mousePosition;
     }
   }
 }
