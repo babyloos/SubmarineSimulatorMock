79dc4918a5982794dc2135858f2f451168fc006a
babyloos <babyloos2501@gmail.com>
2024-07-28 20:29:20 +0900

    駆逐艦の生成移動処理追加

--- Assets/Scripts/EnemyShipManager.cs
+++ Assets/Scripts/EnemyShipManager.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class EnemyShipManager : MonoBehaviour
 {
   public GameObject CargoShipPrefab;
+  public GameObject DestroyerPrefab;
 
   void Start () {
     Instantiate(this.CargoShipPrefab, new Vector3(-10.0f,0.0f,-200.0f), Quaternion.Euler(0, 90f, 0));
-    Instantiate(this.CargoShipPrefab, new Vector3(-10.0f,0.0f,-500.0f), Quaternion.Euler(0, 90f, 0));
-    Instantiate(this.CargoShipPrefab, new Vector3(-10.0f,0.0f,-700.0f), Quaternion.Euler(0, 90f, 0));
+    Instantiate(this.CargoShipPrefab, new Vector3(-10.0f,0.0f,-400.0f), Quaternion.Euler(0, 90f, 0));
+    Instantiate(this.CargoShipPrefab, new Vector3(-10.0f,0.0f,-600.0f), Quaternion.Euler(0, 90f, 0));
+    Instantiate(this.DestroyerPrefab, new Vector3(-10.0f,0.0f,-800.0f), Quaternion.Euler(0, 90f, 0));
   }
 
   void Update()
