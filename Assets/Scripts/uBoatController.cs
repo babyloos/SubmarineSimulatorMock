de6db93aee7ffe9b64d2a746a5888c298cef5019
babyloos <babyloos2501@gmail.com>
2024-12-27 01:57:32 +0900

    インデント調整

--- Assets/Scripts/uBoatController.cs
+++ Assets/Scripts/uBoatController.cs
@@ -6,27 +6,28 @@ using Common;
 
 public class UBoatController : ShipControllerBase
 {
-  public CompassBackController CompassBackController;
-  public GameObject TorpedoPrefab;
+    public CompassBackController CompassBackController;
+    public GameObject TorpedoPrefab;
 
-  void Start()
-  {
-    base.Start();
-  }
+    void Start()
+    {
+        base.Start();
+    }
 
-  void Update()
-  {
-    base.Update();
-  }
+    void Update()
+    {
+        base.Update();
+    }
 
-  protected override void UpdateDirection()
-  {
-    base.UpdateDirection();
-    this.CompassBackController.SetRotate(this._course);
-  }
+    protected override void UpdateDirection()
+    {
+        base.UpdateDirection();
+        this.CompassBackController.SetRotate(this._course);
+    }
 
-  public void FireTorpedo() {
-    var position = (this._transform.position + new Vector3(0, 0, 0)) + this._transform.forward * -38f;
-    Instantiate(this.TorpedoPrefab, position, Quaternion.Euler(this._transform.eulerAngles));
-  }
+    public void FireTorpedo()
+    {
+        var position = (this._transform.position + new Vector3(0, 0, 0)) + this._transform.forward * -38f;
+        Instantiate(this.TorpedoPrefab, position, Quaternion.Euler(this._transform.eulerAngles));
+    }
 }
