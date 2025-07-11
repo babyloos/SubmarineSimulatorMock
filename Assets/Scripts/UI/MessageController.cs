9e3734c6aa2ad054a60a425e5bae029248cb290f
babyloos <babyloos2501@gmail.com>
2025-07-12 00:42:50 +0900

    メッセージ表示時にスクロールバーが一番下へ行くよう修正

--- Assets/Scripts/UI/MessageController.cs
+++ Assets/Scripts/UI/MessageController.cs
@@ -6,25 +6,34 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SocialPlatforms;
+using UnityEngine.UI;
 
 public class MessageController : MonoBehaviour
 {
     public TextMeshProUGUI textMeshPro;
+    private ScrollRect scrollRect;
     private LocalizationManager locale;
 
     void Start()
     {
         locale = LocalizationManager.Instance;
+        if (scrollRect == null)
+        {
+            scrollRect = GetComponentInParent<ScrollRect>();
+        }
     }
 
     public void ShowMessage(ROLE role, List<String> messages)
     {
         var resultMessage = "";
-        foreach (var message in messages) {
+        foreach (var message in messages)
+        {
             var text = this.locale.GetLocalizedText(role.GetStringValue()) + ": " + message;
             resultMessage += text + "\n";
         }
         textMeshPro.text += resultMessage;
+        Canvas.ForceUpdateCanvases();
+        scrollRect.verticalNormalizedPosition = 0f;
     }
 
 }
