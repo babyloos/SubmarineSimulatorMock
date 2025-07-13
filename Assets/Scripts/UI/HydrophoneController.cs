c9fb31e0c67c13570df0421ab9ff65c41496dcb8
babyloos <babyloos2501@gmail.com>
2025-07-13 23:22:48 +0900

    不要なusing削除

--- Assets/Scripts/UI/HydrophoneController.cs
+++ Assets/Scripts/UI/HydrophoneController.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.Localization.Platform.Android;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class hydrophoneController : MonoBehaviour
